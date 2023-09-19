3f2dbe3318717feab120fef2b54bee131ac8a172
Yann Gouffon <github@yago-art.ch>
2023-09-19 15:28:26 +0200

    ♻️ drop Vite for production build and use Rollup/TailwindCSS

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,8 @@ import { render } from 'preact';
 
 import App from './components/App';
 
-import './index.css';
-
 const root = document.createElement('div');
 root.id = 'lkrl-wrapper';
 document.body.appendChild(root);
 
 render(<App />, root);
-
-// Not so nice hack to trick the bundler and avoid $ (jQuery) overrides by libs
-try {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // prettier-ignore
-  // eslint-disable-next-line no-global-assign,@typescript-eslint/no-unused-vars,no-nested-ternary
-  $ = typeof $ !== 'undefined' ? $ : typeof jQuery !== 'undefined' ? jQuery : null;
-  // eslint-disable-next-line no-empty
-} catch (e) {}
