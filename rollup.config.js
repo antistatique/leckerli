3ae5339d8324c0330d0ec51747c85778e6014133
Yann Gouffon <github@yago-art.ch>
2023-09-21 13:58:22 +0200

    🔧 add missing APP_VERSION

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
+import fs from 'fs';
+
+const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
 
 export default [
   { file: './src/index.tsx', name: 'leckerli' },
@@ -20,6 +23,7 @@ export default [
   plugins: [
     replace({
       'process.env.NODE_ENV': JSON.stringify('production'),
+      APP_VERSION: JSON.stringify(version),
     }),
     terser(),
     nodeResolve({ preferBuiltins: true }),
